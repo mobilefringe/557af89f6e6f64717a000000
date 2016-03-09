c1883fbb6023bd8e1f6ad6709c727d8338cbf831
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-03-08 22:01:08 -0500

    Scarborough Town Centre 1.3  /application.js

--- application.js
+++ application.js
@@ -133,6 +133,7 @@ function init_home_hours(){
     $.each( getPropertyHours(), function(i,v){
         if(v.is_holiday == true || v.is_closed == true){
             var hours_day = new Date(v.holiday_date + "T05:00:00Z")
+            hours_day.setHours(0, 0, 0, 0);
             console.log(hours_day)
             
             if(hours_day == d){
