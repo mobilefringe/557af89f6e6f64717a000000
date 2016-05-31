01c4da38de2f8cd2298790ffac3848dd869b9eff
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-05-31 11:58:37 -0400

    Scarborough Town Centre 1.3  /application.js

--- application.js
+++ application.js
@@ -704,7 +704,7 @@ function renderPosts(container, template, collection){
         }
         val.counter = counter;
         var date_blog = new Date(val.publish_date);
-        console.log(date_blog)
+        console.log(val)
         val.published_on = get_month(date_blog.getMonth()) + " " + date_blog.getDate() + ", " + date_blog.getFullYear();
         
         var rendered = Mustache.render(template_html,val);
