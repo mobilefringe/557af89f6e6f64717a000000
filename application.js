46a927f31803613c9e8ef15ec5010e9653332ce3
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-06-06 10:42:56 -0400

    Scarborough Town Centre 1.3  /application.js

--- application.js
+++ application.js
@@ -119,6 +119,7 @@ function init_home_hours(){
     var template_html = $('#home_hours_template').html();
     Mustache.parse(template_html);   // optional, speeds up future uses
     $.each( hours_today , function( key, val ) {
+        console.log(val)
         var open_time = new Date (val.open_time);
         var close_time = new Date (val.close_time);
         val.open = check_open_time(open_time, close_time);
