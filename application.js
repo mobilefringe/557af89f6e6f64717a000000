a8d31d4300b902c2d1b4c2c727759463400bef36
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-06-06 11:48:41 -0400

    Scarborough Town Centre 1.3  /application.js

--- application.js
+++ application.js
@@ -131,7 +131,7 @@ function init_home_hours(){
         if (parseInt(val.close) <10){
             close_t = "0" + val.close
         }
-        console.log("2001-01-01 " + val.close)
+        console.log("2001-01-01 " + close_t)
         var close_in_mins = close.hour()*60 + close.minutes()
         var time_now_in_mins = moment().hour()*60 + moment().minutes()
         console.log(open_in_mins)
