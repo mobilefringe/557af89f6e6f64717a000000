891c2711ae51f898eb4cd914e51dffa0d74646e9
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-06-06 14:10:04 -0400

    Scarborough Town Centre 1.3  /application.js

--- application.js
+++ application.js
@@ -610,7 +610,7 @@ function renderBanner(banner_template,home_banner,banners){
            val.noLink = "return false";
         }
         
-        console.log(val.url)
+        console.log(val)
        if (start <= today){
          if (val.end_date){
              end = new Date (val.end_date);
