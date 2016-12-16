848caba1f666a11ef93857cf89d4dde52f1e3783
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-12-16 12:02:38 -0500

    Scarborough Town Centre 1.6  /helpers.js

--- helpers.js
+++ helpers.js
@@ -125,9 +125,9 @@ function check_open_time(d, close){
     if (current_time >= open && current_time < close){
         time = "OPEN NOW";
     }
-    else{
-        time = "CLOSED";
-    }
+    // else{
+    //     time = "CLOSED";
+    // }
     return time;
 }
 
