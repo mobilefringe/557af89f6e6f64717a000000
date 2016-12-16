cd73faa1a7423ac583a31d9dc161801ab824deb0
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-12-16 12:12:30 -0500

    Scarborough Town Centre 1.6  /application.js

--- application.js
+++ application.js
@@ -120,7 +120,7 @@ function init_home_hours(){
         var open_time = moment(val.open_time).tz(getPropertyTimeZone());
         var close_time = moment(val.close_time).tz(getPropertyTimeZone());
         val.open = check_open_time(open_time, close_time);
-        // val.close = close_time.format("h:mm A");
+        val.close = close_time.format("h:mm A");
        
         var rendered = Mustache.render(template_html,val);
         item_rendered.push(rendered);
