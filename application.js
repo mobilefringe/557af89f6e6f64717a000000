2d2203586335ce4f141a59816191902022099da2
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2017-03-20 11:22:03 -0400

    Scarborough Town Centre 1.3.2  /application.js

--- application.js
+++ application.js
@@ -680,8 +680,8 @@ function renderPosts(container, template, collection){
         val.description_short = val.description_short.replace("&amp;", "&");
         val.slug = "posts/" +val.slug;
         var lb = getBlogDataBySlug("stc-lookbook");
-        // var contest = getBlogDataBySlug("stc-contests");
-        // var out_blog = lb.posts.concat(contest.posts);
+        var contest = getBlogDataBySlug("stc-contests");
+        var out_blog = lb.posts.concat(contest.posts);
         var id = val.id;
         var result = $.grep(out_blog, function(e){ return e.id == id; });
         if(result.length > 0){
