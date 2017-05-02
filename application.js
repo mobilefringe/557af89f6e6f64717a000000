cfe7c3f30158043c2bc7f3cad1d28ec4c2e3afa9
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-05-02 16:24:38 -0400

    Scarborough Town Centre 1.7  /application.js

--- application.js
+++ application.js
@@ -745,8 +745,8 @@ function renderPostDetails(container, template, collection){
         else{
             val.description_short = val.body;
         }
-        var date_blog = new Date((val.publish_date + " 05:00:00").replace(/-/g,"/"));
-        val.published_on = get_month(date_blog.getMonth()) + " " + date_blog.getDate() + ", " + date_blog.getFullYear();
+        var date_blog = moment(val.publish_date).tz(getPropertyTimeZone());
+        val.published_on = date_blog.format('MMM DD, YYYY');
         var next_p = getNextPublishedPostBySlug(val.slug);
         var prev_p = getPrevPublishedPostBySlug(val.slug);
         if (next_p == undefined){
