8a15d7296f31e9010d5f49b3c886c67447b2a667
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-08 16:14:30 -0400

    Scarborough Town Centre 1.8  /application.js

--- application.js
+++ application.js
@@ -141,6 +141,7 @@ function init_home_hours(){
             var today = moment().tz(getPropertyTimeZone()).format("MMM DD YYYY");
             
             if(hours_day === today){
+                console.log(v.is_holiday, v.is_closed);
                 var open_time = moment(v.open_time).tz(getPropertyTimeZone());
                 var close_time = moment(v.close_time).tz(getPropertyTimeZone());
                 v.open = check_open_time(open_time, close_time);
